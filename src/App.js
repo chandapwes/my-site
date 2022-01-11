eb1135578d8163f6ca9b931f61b1e524dc8778c2
chandapwes <67353485+chandapwes@users.noreply.github.com>
2022-01-11 16:59:27 -0700

    basic structure and routes made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home'
+import InfoPage from './components/InfoPage';
+import Projects from './components/Projects';
+import ContactPage from './components/ContactPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <p>
+        Chandler's Site
+      </p>
+
+      
+    </header>
+
+      <div>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/info'>About Me</Link>
+          <Link to='/projects'>My Projects</Link>
+          <Link to='/contact'>How to Reach Me</Link>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/info' element={<InfoPage/>} />
+        <Route exact path='/projects' element={<Projects/>} />
+        <Route exact path='/contact' element={<ContactPage/>} />
+      </Routes>
+
+    </>
   );
 }
 
